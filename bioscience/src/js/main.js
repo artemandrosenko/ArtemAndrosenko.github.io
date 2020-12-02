3fe96e11ff9b79416d6bd736bd40049ea8440f35
Artem Androsenko <hi.tema@outlook.com>
2020-12-02 18:57:54 +0200

    Add about-us section

--- bioscience/src/js/main.js
+++ bioscience/src/js/main.js
@@ -1,10 +1,22 @@
 const body = document.querySelector('body');
 const hamburgerBtn = document.querySelector('.header__hamburger');
 const headerNav = document.querySelector('.header__nav');
+const navItem = document.querySelectorAll('.nav__item');
 
 hamburgerBtn.addEventListener('click', () => {
     hamburgerBtn.classList.toggle('open');
     headerNav.classList.toggle('active');
     
     headerNav.classList.contains('active') ? body.style.cssText = 'overflow: hidden' : body.style.cssText = 'overflow: visible';
+});
+
+navItem.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        console.log(item);
+        if (event.target instanceof HTMLAnchorElement) {
+            hamburgerBtn.classList.remove('open');
+            headerNav.classList.remove('active');
+            body.style.cssText = 'overflow: visible'
+        }
+    });
 });
